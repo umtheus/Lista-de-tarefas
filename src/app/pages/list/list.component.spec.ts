adb6757062bb17af6957d288a3196b0abb58163e
umtheus <gm.arr.matheus@gmail.com>
2025-10-07 13:27:03 -0300

    test(ListComponent):implementa testa da listagem de tarefas

--- src/app/pages/list/list.component.spec.ts
+++ src/app/pages/list/list.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
+import { By } from '@angular/platform-browser';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -15,7 +16,29 @@ describe('ListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('deve listar as tarefas', () => {
+    const todoSection = fixture.debugElement.query(
+      By.css('[data-testid="todo-list"]')
+    );
+
+    expect(todoSection).toBeTruthy();
+
+    const todoItems = todoSection.queryAll(
+      By.css('[data-testid="todo-list-item"]')
+    );
+
+    expect(todoItems.length).toBe(3);
+
+    const completedSection = fixture.debugElement.query(
+      By.css('[data-testid="completed-list"]')
+    );
+
+    expect(completedSection).toBeTruthy();
+
+    const completedItems = completedSection.queryAll(
+      By.css('[data-testid="completed-list-item"]')
+    );
+
+    expect(completedItems.length).toBe(3);
   });
 });
