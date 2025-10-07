8817acd1485fef6bdcd6ee64f9dbf52b889f7c00
umtheus <gm.arr.matheus@gmail.com>
2025-10-07 13:26:16 -0300

    feat(ListComponent):implementa listagem de tarefas de forma fixa usando Tailwind

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+interface Task {
+  title: string;
+  completed: boolean;
+}
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -8,4 +12,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent {
+  tasks = signal<Task[]>([
+    { title: 'Item 1', completed: false },
+    { title: 'Item 2', completed: false },
+    { title: 'Item 3', completed: false },
+    { title: 'Item 4', completed: true },
+    { title: 'Item 5', completed: true },
+    { title: 'Item 6', completed: true },
+  ]);
+
+  completedTasks = computed(() =>
+    this.tasks().filter((task) => task.completed)
+  );
+  pendingTasks = computed(() => this.tasks().filter((task) => !task.completed));
+}
